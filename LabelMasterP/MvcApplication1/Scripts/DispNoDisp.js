ec2fc8807c75544a999651fce0e7a52a71975b19
MauAguirre <murriaguirre@gmail.com>
2016-05-31 18:31:21 -0500

    Redesign-JS

--- LabelMasterP/MvcApplication1/Scripts/DispNoDisp.js
+++ LabelMasterP/MvcApplication1/Scripts/DispNoDisp.js
@@ -1,7 +1,7 @@
 ﻿    function toggle_visibility(id) {
     	var e = document.getElementById(id);
-    	if(e.style.display == 'block')
-    		e.style.display = 'none';
-    	else
+    	if(e.style.display == 'none')
     		e.style.display = 'block';
+    	else
+    		e.style.display = 'none';
     }
